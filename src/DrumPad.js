d772ca9dfd1eb32c1d7c4acbb576e26c3bfc36a6
Richmond S. Damag <rsdamag.cpa.8@gmail.com>
2022-10-29 20:14:56 +0800

    DrumPad component enhancements

--- src/DrumPad.js
+++ src/DrumPad.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-const DrumPad = () => {
+const DrumPad = (props) => {
+  const [drumbeat, setDrumbeat] = useState("");
+  const onClickHandler_beat = (event) => {
+    //
+
+    setDrumbeat(`${event.currentTarget.id}x`);
+    console.log(drumbeat);
+    document.getElementById(drumbeat).play();
+  };
+
+  //const onKeyPressHandler = (event) => {};
+
   return (
-    <button className="drum-pad" id="Heater-1">
-      <audio
-        id="Q"
-        src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
-        className="clip"
-      ></audio>
-      Q
-    </button>
+    <div id="drum-machine">
+      {props.items.map((elem) => (
+        <button
+          key={elem.key}
+          className="drum-pad"
+          id={elem.key}
+          onClick={onClickHandler_beat}
+        >
+          <audio id={`${elem.key}x`} src={elem.url} className="clip"></audio>
+          {elem.key}
+        </button>
+      ))}
+    </div>
   );
 };
 
---
+++ src/app_backup.js
@@ -0,0 +1,224 @@
+import React, { useState } from "react";
+import "./App.css";
+import DrumPad from "./DrumPad";
+
+import Heater_1 from "./audio/Heater-1.mp3";
+import Heater_2 from "./audio/Heater-2.mp3";
+import Heater_3 from "./audio/Heater-3.mp3";
+import Heater_4 from "./audio/Heater-4.mp3";
+import Clap from "./audio/Clap.mp3";
+import Open_HH from "./audio/Open_HH.mp3";
+import Kick_n_Hat from "./audio/Kick_n_Hat.mp3";
+import Kick from "./audio/Kick.mp3";
+import Closed_HH from "./audio/Closed_HH.mp3";
+
+const Q_audio = new Audio(Heater_1);
+const W_audio = new Audio(Heater_2);
+const E_audio = new Audio(Heater_3);
+const A_audio = new Audio(Heater_4);
+const S_audio = new Audio(Clap);
+const D_audio = new Audio(Open_HH);
+const Z_audio = new Audio(Kick_n_Hat);
+const X_audio = new Audio(Kick);
+const C_audio = new Audio(Closed_HH);
+var drum_spec;
+
+const DRUM_CONFIG = [
+  {
+    key: "Q",
+    song: "Heater-1",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  },
+  {
+    key: "W",
+    song: "Heater-2",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  },
+  {
+    key: "E",
+    song: "Heater-3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  },
+  {
+    key: "A",
+    song: "Heater-4",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  },
+  {
+    key: "S",
+    song: "Heater-6",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  },
+  {
+    key: "D",
+    song: "Dsc_Oh",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  },
+  {
+    key: "Z",
+    song: "Kick_n_Hat",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  },
+  {
+    key: "X",
+    song: "RP4_KICK_1",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  },
+  {
+    key: "C",
+    song: "Cev_H2",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+  },
+];
+
+function App() {
+  const [drum_specState, setDrum_specState] = useState(drum_spec);
+
+  const Q_beat = (event) => {
+    Q_audio.play();
+    setDrum_specState("Heater_1");
+  };
+
+  const W_beat = (event) => {
+    W_audio.play();
+    W_audio.currentTime = 0;
+    setDrum_specState("Heater_2");
+  };
+
+  const E_beat = (event) => {
+    E_audio.play();
+    E_audio.currentTime = 0;
+    setDrum_specState("Heater_3");
+  };
+
+  const A_beat = (event) => {
+    A_audio.play();
+    A_audio.currentTime = 0;
+    setDrum_specState("Heater_4");
+  };
+
+  const S_beat = (event) => {
+    S_audio.play();
+    S_audio.currentTime = 0;
+    setDrum_specState("Clap");
+  };
+
+  const D_beat = (event) => {
+    D_audio.play();
+    D_audio.currentTime = 0;
+    setDrum_specState("Open High Hat");
+  };
+
+  const Z_beat = (event) => {
+    Z_audio.play();
+    Z_audio.currentTime = 0;
+    setDrum_specState("Kick n' Hat");
+  };
+
+  const X_beat = (event) => {
+    X_audio.play();
+    X_audio.currentTime = 0;
+    setDrum_specState("Kick!");
+  };
+
+  const C_beat = (event) => {
+    C_audio.play();
+    C_audio.currentTime = 0;
+    setDrum_specState("Closed High Hat");
+  };
+
+  const qwerty = (event) => {
+    switch (event.key) {
+      case "q":
+        Q_beat();
+        break;
+      case "w":
+        W_beat();
+        break;
+      case "e":
+        E_beat();
+        break;
+
+      case "a":
+        A_beat();
+        break;
+      case "s":
+        S_beat();
+        break;
+      case "d":
+        D_beat();
+        break;
+
+      case "z":
+        D_beat();
+        break;
+
+      case "x":
+        X_beat();
+        break;
+
+      case "c":
+        C_beat();
+        break;
+      default:
+    }
+  };
+
+  window.addEventListener("keypress", qwerty);
+
+  return (
+    <div className="Table1">
+      <div id="drum-machine">
+        <DrumPad />
+        <button className="drum-pad" id="Heater-1" onClick={Q_beat}>
+          <audio
+            id="Q"
+            src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+            className="clip"
+          ></audio>
+          Q
+        </button>
+        <button className="drum-pad" id="Heater-2" onClick={W_beat}>
+          W{" "}
+          <audio
+            id="W"
+            src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+            className="clip"
+          ></audio>
+        </button>
+
+        <button className="drum-pad" id="Heater-3" onClick={E_beat}>
+          E <audio id="E" src="./Heater-1.mp3" className="clip"></audio>
+        </button>
+
+        <button className="drum-pad" id="Heater-4" onClick={A_beat}>
+          A <audio id="A" src="./Heater-1.mp3" className="clip"></audio>
+        </button>
+
+        <button className="drum-pad" id="Clap" onClick={S_beat}>
+          S <audio id="S" src="./Heater-1.mp3" className="clip"></audio>
+        </button>
+
+        <button className="drum-pad" id="Open-HH" onClick={D_beat}>
+          D <audio id="D" src="./Heater-1.mp3" className="clip"></audio>
+        </button>
+
+        <button className="drum-pad" id="Kick-n-Hat" onClick={Z_beat}>
+          Z <audio id="Z" src="./Heater-1.mp3" className="clip"></audio>
+        </button>
+
+        <button className="drum-pad" id="Kick" onClick={X_beat}>
+          X <audio id="X" src="./Heater-1.mp3" className="clip"></audio>
+        </button>
+
+        <button className="drum-pad" id="Close-HH" onClick={C_beat}>
+          C <audio id="C" src="./Heater-1.mp3" className="clip"></audio>
+        </button>
+      </div>
+
+      <div id="display">{String(drum_specState)}</div>
+    </div>
+  );
+}
+
+export default App;
