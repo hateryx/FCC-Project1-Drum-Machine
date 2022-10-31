47552712888d864f13dee22a5225da38f1861004
Richmond S. Damag <rsdamag.cpa.8@gmail.com>
2022-10-31 21:30:36 +0800

    Update DrumPad.js

--- src/DrumPad.js
+++ src/DrumPad.js
@@ -2,12 +2,24 @@ import React, { useState } from "react";
 
 const DrumPad = (props) => {
   const [drumbeat, setDrumbeat] = useState("");
+
+  const onKeyPressHandler_beat = (event) => {
+    setDrumbeat(event.key);
+    //document.getElementById(drumbeat).play();
+    //document.getElementById(drumbeat).currentTime = 0;
+    console.log(event.target);
+    //document.getElementById(pad.song).click();
+  };
+
+  window.addEventListener("keypress", onKeyPressHandler_beat);
+
   const onClickHandler_beat = (event) => {
     //
 
-    setDrumbeat(`${event.currentTarget.id}x`);
-    console.log(drumbeat);
+    setDrumbeat(event.currentTarget.id.charAt(0));
+    console.log(event.target);
     document.getElementById(drumbeat).play();
+    document.getElementById(drumbeat).currentTime = 0;
   };
 
   //const onKeyPressHandler = (event) => {};
@@ -17,16 +29,16 @@ const DrumPad = (props) => {
       {props.items.map((elem) => (
         <button
           key={elem.key}
+          id={`${elem.key}x`}
           className="drum-pad"
-          id={elem.key}
           onClick={onClickHandler_beat}
         >
-          <audio id={`${elem.key}x`} src={elem.url} className="clip"></audio>
+          <audio id={elem.key} src={elem.url} className="clip"></audio>
           {elem.key}
         </button>
       ))}
     </div>
   );
 };
-
+//
 export default DrumPad;
